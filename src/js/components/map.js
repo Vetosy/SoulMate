6b068892af02c47d1ea016a2fb07209d64efb816
Vitalii Stepnov <97784759+Vetosy@users.noreply.github.com>
2023-07-05 16:32:41 +0300

    added footer

--- src/js/components/map.js
+++ src/js/components/map.js
@@ -2,11 +2,11 @@ import ymaps from 'ymaps';
 
 ymaps.load('https://api-maps.yandex.ru/2.1/?apikey=5f264f26-e07e-4f77-a379-3b0fa55eb0ab&lang=ru_RU').then(maps => {
   const map = new maps.Map('map', {
-    center: [55.02668469359501, 82.92077705377105],
-    zoom: 17,
+    center: [55.026627398452774, 82.92078348585451],
+    zoom: 18,
   });
 
-  const placemark = new maps.Placemark([55.02668469359501, 82.92077705377105], {
+  const placemark = new maps.Placemark([55.026627398452774, 82.92078348585451], {
     hintContent: 'Soulmate',
     balloonContentHeader: 'г.Новосибирск',
     balloonContentBody: 'ул. Красный проспект, 17',
