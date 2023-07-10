ed04c10d7ac348674c6ab5706177c4acbad18e0b
Vitalii Stepnov <97784759+Vetosy@users.noreply.github.com>
2023-07-10 21:31:44 +0300

    fix#2

--- src/js/components/map.js
+++ src/js/components/map.js
@@ -1,28 +1,29 @@
-const center = [55.02662761597713, 82.9207828920306]
+const center = [55.02662761597713, 82.9207828920306];
 
 function init() {
   const map = new ymaps.Map('map', {
     center: center,
     zoom: 18,
-  })
+  });
 
   const placemark = new ymaps.Placemark(center, {
-
     balloonContentHeader: 'г.Новосибирск',
     balloonContentBody: 'ул. Красный проспект, 17',
     balloonContentFooter: '8 906-907-64-58',
   }, {
     iconLayout: 'default#image',
-    iconImageHref: '../../img/marker.svg',
+    iconImageHref: 'img/marker.svg',
     iconImageSize: [40, 40],
     iconImageOffset: [-20, -40],
-  })
+  });
 
   map.controls.remove('trafficControl');
   map.controls.remove('typeSelector');
   map.controls.remove('rulerControl');
 
-  map.geoObjects.add(placemark)
+  map.geoObjects.add(placemark);
 }
 
-ymaps.ready(init);
+document.addEventListener('DOMContentLoaded', () => {
+  ymaps.ready(init);
+});
